06fec3a782f0077c20e6e21f510f2e8806635f5e
souravsarker2015 <souravsarker2015@gmail.com>
2023-05-10 02:06:18 +0600

    home page designed

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,10 +1,16 @@
+import {Hero} from "./components/Hero";
+import {FeatureProducts} from "./components/FeatureProducts";
+import {Testimonials} from "./components/Testimonials";
+import {Faq} from "./components/Faq";
+
 export const HomePage = () => {
     return (
 
         <main>
-            <div>
-                homepage
-            </div>
+            <Hero/>
+            <FeatureProducts/>
+            <Testimonials/>
+            <Faq/>
 
         </main>
     );
